5e4cf30db1dee31865a5146d7a077c1625335ac9
Manuele Pesenti <manuele.pesenti@gter.com>
2022-03-19 00:02:43 +0100

    first commit: frontend troubles

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simile
+
+1. clone this repo
+
+2. add `settings_private.py` configuration file for backend app in root dir
+
+3. run `./compose`
---
+++ compose.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export BACKENDAPP=istsos;
+export FRONTENDAPP=simile;
+
+build=0;
+detach=0;
+while getopts "hbd" arg; do
+  case $arg in
+    h)
+      echo "Sorry, no help available." ;
+      exit 0;
+      ;;
+    b)
+      build=1;
+      ;;
+    d)
+      detach=1;
+      ;;
+  esac
+done
+
+if [ ! -d "./${BACKENDAPP}" ]; then
+    git clone https://github.com/gtergeomatica/istsosproxy.git $BACKENDAPP;
+    # 
+    cp ./settings_private.py $BACKENDAPP/
+fi
+
+if [ ! -d "./${FRONTENDAPP}" ]; then
+    git clone --branch frontend https://github.com/gtergeomatica/template_progetto_simile.git $FRONTENDAPP;
+fi
+
+if [ $build -eq 1 ]; then
+    echo "Building"
+    docker compose build;
+elif [ $detach -eq 1 ]; then
+    echo "Detaching"
+    docker compose up -d;
+else
+    echo "Debug"
+    docker compose up;
+fi;
